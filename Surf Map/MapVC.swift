d1ff8a4e7d776e95ec4e2a5b597313b0bd04ffbb
Patrick Woo-Sam <pwoosam5@icloud.com>
2017-01-24 00:50:02 -0800

    Map loads and displays current location

--- Surf Map/MapVC.swift
+++ Surf Map/MapVC.swift
@@ -6,7 +6,25 @@
 //  Copyright © 2017 Patrick Woo-Sam. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import GoogleMaps
 
-let API_KEY = "AIzaSyCaqDP-rJxhFzCAAWmCS6X0IrWKOOnBCsY"
+class MapVC: UIViewController {
+
+    @IBOutlet var mapView_: GMSMapView!
+
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var mapView: GMSMapView!
+    var zoomLevel: Float = 12.0
+
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display
+        // Santa Barbara, California with some zoom.
+        let camera = GMSCameraPosition.camera(withLatitude: 34.4133, longitude: -119.8610, zoom: self.zoomLevel)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        self.view = mapView
+    }
+}
